b4084c7fcb40bbd5f429cd8cf62b5165393d76d0
Deep in misery <131404446+Betus971@users.noreply.github.com>
2025-01-21 11:03:18 +0100

    debut

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module ppe6.ppe6 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens ppe6.ppe6 to javafx.fxml;
+    exports ppe6.ppe6;
+}
---
+++ src/main/java/ppe6/ppe6/HelloController.java
@@ -0,0 +1,14 @@
+package ppe6.ppe6;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
